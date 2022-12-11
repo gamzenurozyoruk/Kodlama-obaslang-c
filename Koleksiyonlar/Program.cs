45e1c9bc8de3e83bf94c679f0195d18c6c1e1e2b
gamzenurozyoruk <gamze190499.34@hotmail.com>
2022-12-11 11:15:56 +0300

    11.12.2022 GenericIntro

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List
+        }
+    }
+}
