425a8da4c8a862311698e0b9878e864685246e7d
gamzenurozyoruk <gamze190499.34@hotmail.com>
2022-12-11 10:54:39 +0300

    ClassMethodDemo

---
+++ ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri MusteriEkleme)
+        {
+            Console.WriteLine("Müşteri başarılı şekilde eklendi" + MusteriEkleme.MusteriAdi);
+            Console.WriteLine("Müşteri başarılı şekilde eklendi" + MusteriEkleme.MusteriId);
+            Console.WriteLine("Müşteri başarılı şekilde eklendi" + MusteriEkleme.MusteriSoyadi);
+            Console.WriteLine("Müşteri başarılı şekilde eklendi" + MusteriEkleme.MusteriYas);
+            Console.WriteLine("Müşteri başarılı şekilde eklendi" + MusteriEkleme.TcKimlikNumarası);
+        }
+
+        public void Silme(Musteri MusteriSilme)
+        {
+            Console.WriteLine("Müşteri başarılı şekilde silindi" + MusteriSilme.MusteriAdi);
+            Console.WriteLine("Müşteri başarılı şekilde silindi" + MusteriSilme.MusteriSoyadi);
+            Console.WriteLine("Müşteri başarılı şekilde silindi" + MusteriSilme.TcKimlikNumarası);
+            Console.WriteLine("Müşteri başarılı şekilde silindi" + MusteriSilme.MusteriId);
+            Console.WriteLine("Müşteri başarılı şekilde silindi" + MusteriSilme.MusteriYas);
+            
+        }
+
+
+
+        public void Listele(params Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Id : " + musteri.MusteriId + "\n" + "Tc : " + musteri.TcKimlikNumarası + "\n" + "Müşteri Adı : " +
+                    musteri.MusteriAdi + "\n" + "Müşteri Soyadı : " + musteri.MusteriSoyadi);
+                Console.WriteLine("----------------------------------");
+            }
+            
+        //public void Listele(Musteri MusteriListele)
+        //{
+        //    for (int i = 0; i < musteriler.Length; i++)
+        //    {
+        //        if (musteriler[i] != null)
+        //        {
+                  
+        //            Console.WriteLine("ID: " + MusteriListele.MusteriId + "Tc Kimlik Numarası: " + MusteriListele.TcKimlikNumarası + "Adı: " + MusteriListele.MusteriAdi + "Soyadı: " + MusteriListele.MusteriSoyadi + "Yaşı: " + MusteriListele.MusteriYas);
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+
+        //    }
+
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+
+            musteri.MusteriAdi = "Gamzenur";
+            musteri.MusteriSoyadi = "Özyörükoğulları";
+            musteri.TcKimlikNumarası = "35764513688";
+            musteri.MusteriId = 1;
+            musteri.MusteriYas = 23;
+
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "Hatice";
+            musteri1.MusteriSoyadi = "Özyörükoğulları";
+            musteri1.TcKimlikNumarası = "10022012288";
+            musteri1.MusteriId = 2;
+            musteri1.MusteriYas = 50;
+            
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Tuğçe";
+            musteri2.MusteriSoyadi = "Özyörükoğulları";
+            musteri2.TcKimlikNumarası = "1023447654343";
+            musteri2.MusteriId = 3;
+            musteri2.MusteriYas = 18;
+
+            //Musteri musteri = new Musteri() { MusteriId = 1, TcKimlikNumarası = "35764513688", MusteriAdi = "Gamzenur", MusteriSoyadi = "Özyörükoğulları",MusteriYas = 23 }; böyle de tanımlanabilir
+
+            Musteri[] musteriler = new Musteri[] {musteri,musteri1,musteri2};
+            //Musteri musteri1 = new Musteri() {MusteriId = 2, TcKimlikNumarası = "10022012288", MusteriAdi = "Hatice", MusteriSoyadi = "Özyörükoğulları", MusteriYas = 50 }; böyle de tanımlanabilir
+         
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Silme(musteri);
+            musteriManager.Listele(musteriler);
+            musteriManager.Silme(musteri2);
+
+        }
+    }
+}
